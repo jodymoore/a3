3528c909ca4139fe4c7305c91f040fef0354650f
Jody Moore <jodymoore@g.harvard.edu>
2017-03-11 10:35:18 -0600

    change to packages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+*  Book related routes
+*/
+Route::get('/books', 'BookController@index');
+
+Route::get('/books/{titile?}', 'BookController@view');
+
+if(config('app.env') == 'local') {
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
+
+/**
+*  Practice
+*/
+Route::any('/practice/{n?}', 'PracticeController@index');
+
+/**
+*  Main home page visitors see when they visit just /
+*/
+Route::get('/', 'WelcomeController');
