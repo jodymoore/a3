0fad6be51ee3baa775490346f5f922872dc9355e
Jody Moore <jodymoore@g.harvard.edu>
2017-03-27 21:10:33 -0500

    change to index

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -5,14 +5,10 @@
 
 @include('header')
 
-@if($submitted)
-
 <div class="alert alert-success">
     Your password:<br>
     <h4><strong> {{ $password }} </strong></h4>                  
 </div>
-
-@endif
 
 <input type="button" name="Reset" onclick="window.location='{{ url("/pswdgen") }}'" value="Reset" class='btn btn-primary  btn-small'>
 
