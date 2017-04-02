709c9ee6523b0d10542a315aafa8898743390080
Jody Moore <jodymoore@g.harvard.edu>
2017-04-02 09:40:15 -0500

    styles.css

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -7,9 +7,9 @@
 
 <div class="alert alert-success">
     Your password:<br>
-    <h4><strong> {{ $password }} </strong></h4>                  
+    <h4 id="results"><strong> {{ $password }} </strong></h4>                  
 </div>
 
-<input type="button" name="Reset" onclick="window.location='{{ url("/pswdgen") }}'" value="Reset" class='btn btn-primary  btn-small'>
+<input id="resultReset" type="button" name="Reset" onclick="window.location='{{ url("/pswdgen") }}'" value="Reset" class='btn btn-primary  btn-small'>
 
 @endsection
